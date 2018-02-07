17e43ade347020b737e34510a9cde0783f49e436
riyane <sidlakhdar.riyane@gmail.com>
2018-02-07 22:41:29 +0100

    Unstable version: password fetch not working

--- updateCritical.sh
+++ updateCritical.sh
@@ -1,12 +1,19 @@
 #!/bin/bash
 
-
+#---------------------------------------
+# User options
+#---------------------------------------
 OPTION_CIPHER="--cipher"
 OPTION_DECIPHER="--decipher"
 
-DIR_NAME="papers"
-COMPRESS_NAME="${DIR_NAME}.zip"
-CIPHERED_NAME="${COMPRESS_NAME}.gpg"
+#---------------------------------------
+# Global parameters
+#---------------------------------------
+PATH_BASE="papers"
+PATH_CIPHERED="papers_ciphered"
+
+
+#PASS="qwerty"
 
 myEcho()
 {
@@ -14,20 +21,50 @@ myEcho()
 }
 
 
+getPass()
+{
+	# password storage
+	PASS=$(tempfile 2>/dev/null)
+
+	# trap it
+	trap "rm -f $PASS" 0 1 2 5 15
+
+	# get password
+	dialog	--title "Password" \
+		--clear \
+		--passwordbox "Enter your password" 10 30 2  > $PASS
+ret=$?
+echo "SSSSS"
+#	clear
+}
+
 cipher()
 {
-	zip -er ${COMPRESS_NAME} ${DIR_NAME}
-	gpg --cipher-algo AES256 -c ${COMPRESS_NAME}
-	rm -f ${COMPRESS_NAME}
+	for file in `ls ${PATH_BASE}`;
+	do
+		echo "+++++++++++++++++++++"
+		echo "ZIP: file ${file}"
+                echo "+++++++++++++++++++++"
+		zip -P ${PASS} -r ${file}.zip ${PATH_BASE}/${file}
+                echo "+++++++++++++++++++++"
+                echo "GPG: pass ${PASS}"
+                echo "+++++++++++++++++++++"
+		echo ${PASS} | gpg --passphrase-fd 0 --cipher-algo AES256 -c --output ${PATH_CIPHERED}/${file}.gpg ${file}.zip
+		rm -f ${file}.zip
+	done;
+}
 
+
+deleteClearFic()
+{
 	while true; do
-		myEcho "Confirm suppression of \"${DIR_NAME}\" [Y/n]:"
+		myEcho "Confirm suppression of \"${PATH_BASE}\" [Y/n]:"
 		read answer
 		if [ ${answer} == "Y" ] || [ ${answer} == "y" ]; then
 			rm -rf ${DIR_NAME}
-			myEcho "File \"${DIR_NAME}\" deleted"
+			myEcho "File \"${PATH_BASE}\" deleted"
 		elif [ ${answer} == "N" ] || [ ${answer} == "n" ]; then
-	                myEcho "File \"${DIR_NAME}\" not deleted (carefull when u commit)"
+	                myEcho "File \"${PATH_BASE}\" not deleted (carefull when u commit)"
 		else
 			continue
 		fi
@@ -43,11 +80,24 @@ decipher()
 }
 
 
+errorExit()
+{
+	 myEcho "Usage: ${0} [${OPTION_CIPHER} | ${OPTION_DECIPHER}]"
+	exit
+}
 
-if [ ${1} == ${OPTION_CIPHER} ]; then
+if [ $# -ne 1 ]; then
+	errorExit
+elif [ ${1} == ${OPTION_CIPHER} ]; then
+	getPass
 	cipher
+	deleteClearFic
 elif [ ${1} == ${OPTION_DECIPHER} ]; then
-	decipher
+#	decipher ${PATH_BASE}
+echo "AAAAAAAAAAAAAAAAAAAA"
 else
-	myEcho "Usage: ${0} [${OPTION_CIPHER} | ${OPTION_DECIPHER}]"
+#	errorExit
+echo "XXXXXXXXXXXXXxxxxxxx"
 fi
+
+
